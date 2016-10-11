26713178f8c37499ba6c4d747e6465e3081a878a
Joe Marrama <jmarrama@gmail.com>
2016-10-11 12:47:00 -0700

    Basic parsing of json implemented

--- src/s3fuse.go
+++ src/s3fuse.go
@@ -13,22 +13,15 @@ import (
 	"bazil.org/fuse"
 	"bazil.org/fuse/fs"
 	"golang.org/x/net/context"
+	"io/ioutil"
+	"encoding/json"
 )
-
-type FS struct {
-	archive *zip.Reader
-}
-
-var _ fs.FS = (*FS)(nil)
-
-
-// We assume the zip file contains entries for directories too.
 
 var progName = filepath.Base(os.Args[0])
 
 func usage() {
 	fmt.Fprintf(os.Stderr, "Usage of %s:\n", progName)
-	fmt.Fprintf(os.Stderr, "  %s ZIP MOUNTPOINT\n", progName)
+	fmt.Fprintf(os.Stderr, "  %s JsonConfFile MOUNTPOINT\n", progName)
 	flag.PrintDefaults()
 }
 
@@ -52,6 +45,20 @@ func main() {
 
 func mount(path, mountpoint string) error {
 
+	// Rough outline:
+	// TODO - make conf format and parse it and dump it all in a struct
+	// TODO - hook it all into the fuse FS interface
+	// TODO - actually implement reading files from S3 w/ readahead and other stuff in conf
+
+	rootFs, err := LoadFileSystem(path)
+	if err != nil {
+		return err
+	}
+	fmt.Println(rootFs)
+	return nil
+
+
+	/*
 	archive, err := zip.OpenReader(path)
 	if err != nil {
 		return err
@@ -78,10 +85,18 @@ func mount(path, mountpoint string) error {
 	}
 
 	return nil
+	*/
 }
+
+type FS struct {
+	archive *zip.Reader
+}
+
+var _ fs.FS = (*FS)(nil)
 
 
 func (f *FS) Root() (fs.Node, error) {
+	// TODO - store root s3 virtual dir in the FS struct
 	n := &Dir{
 		archive: f.archive,
 	}
@@ -107,6 +122,7 @@ func zipAttr(f *zip.File) fuse.Attr {
 }
 
 func (d *Dir) Attr(ctx context.Context, attr *fuse.Attr) error {
+	// TODO - just return the one for root here
 	if d.file == nil {
 		// root directory
 		*attr = fuse.Attr{Mode: os.ModeDir | 0755}
@@ -119,6 +135,7 @@ func (d *Dir) Attr(ctx context.Context, attr *fuse.Attr) error {
 var _ = fs.NodeRequestLookuper(&Dir{})
 
 func (d *Dir) Lookup(ctx context.Context, req *fuse.LookupRequest, resp *fuse.LookupResponse) (fs.Node, error) {
+	// TODO - split path into parts, simply traverse from current dir onwards
 	path := req.Name
 	if d.file != nil {
 		path = d.file.Name + path
@@ -148,6 +165,7 @@ type File struct {
 var _ fs.Node = (*File)(nil)
 
 func (f *File) Attr(ctx context.Context, attr *fuse.Attr) error {
+	// TODO - just return read only 444....
 	*attr = zipAttr(f.file)
 	return nil
 }
@@ -192,6 +210,7 @@ func (fh *FileHandle) Read(ctx context.Context, req *fuse.ReadRequest, resp *fus
 var _ = fs.HandleReadDirAller(&Dir{})
 
 func (d *Dir) ReadDirAll(ctx context.Context) ([]fuse.Dirent, error) {
+	// TODO - simply just hand back merged lists of files and directories
 	prefix := ""
 	if d.file != nil {
 		prefix = d.file.Name
@@ -221,4 +240,84 @@ func (d *Dir) ReadDirAll(ctx context.Context) ([]fuse.Dirent, error) {
 		res = append(res, de)
 	}
 	return res, nil
+}
+
+
+
+type JsonFileNode struct {
+	Inode int `json:inode`
+	IsDir int `json:isdir`
+	Name string `json:name`
+	Parent int `json:parent`
+	Url string `json:url`
+}
+
+type JsonFileNodes struct {
+	Nodes []JsonFileNode
+}
+
+type S3File struct {
+	Inum int
+	Name string
+	ParentDirInum int
+	Url string
+}
+
+type S3VirtualDir struct {
+	Inum int
+	Name string
+	ParentDirInum int
+	ChildFiles []S3File
+	ChildDirs []S3VirtualDir
+}
+
+func LoadFileSystem(jsonPath string) (S3VirtualDir, error) {
+	bytes, err := ioutil.ReadFile(jsonPath)
+	if err != nil {
+		return S3VirtualDir{}, err
+	}
+
+	nodes := make([]JsonFileNode, 0)
+	err = json.Unmarshal(bytes, &nodes)
+	if err != nil {
+		return S3VirtualDir{}, err
+	}
+
+	// build map of inum -> fileNodes, then build file tree starting from root node
+	// extracting things from the map
+	nodeMap := make(map[int]JsonFileNode)
+	for _, node := range nodes {
+		nodeMap[node.Inode] = node
+	}
+
+	root := nodeMap[0] // deal with error case when root isnt specified some other time
+	return buildDirNode(root, nodeMap), nil
+}
+
+func buildDirNode(curNode JsonFileNode, nodeMap map[int]JsonFileNode) S3VirtualDir {
+	childDirs := []S3VirtualDir{}
+	childFiles := []S3File{}
+
+	for _, node := range nodeMap {
+		if node.Parent == curNode.Inode {
+			if node.IsDir == 1 {
+				childDirs = append(childDirs, buildDirNode(node, nodeMap))
+			} else {
+				childFiles = append(childFiles, S3File{
+					Name: node.Name,
+					Url: node.Url,
+					ParentDirInum: curNode.Inode,
+					Inum: node.Inode,
+				})
+			}
+		}
+	}
+
+	return S3VirtualDir{
+		Name: curNode.Name,
+		Inum: curNode.Inode,
+		ParentDirInum: curNode.Parent,
+		ChildDirs: childDirs,
+		ChildFiles: childFiles,
+	}
 }
